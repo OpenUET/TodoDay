d65dc4c86aa425e0c18c16ca730be0b357130cd1
Trần Tuấn Anh <tuananhniichan@gmail.com>
2023-06-27 00:53:48 +0700

    Create Task button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,10 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import mongoLogo from './assets/mongodb.svg'
-import nodeLogo from './assets/nodejs.svg'
-import reactLogo from './assets/react.svg'
-import Profile from './components/Profile'
-import TaskWeight from './components/TaskWeight'
 import { handleGetUsersAPI } from './services/user'
-import viteLogo from '/vite.svg'
-
-const logos = [
-  {
-    name: 'vite',
-    image: viteLogo,
-    link: 'https://vitejs.dev'
-  },
-  {
-    name: 'react',
-    image: reactLogo,
-    link: 'https://react.dev'
-  },
-  {
-    name: 'nodejs',
-    image: nodeLogo,
-    link: 'https://nodejs.dev/en/'
-  },
-  {
-    name: 'mongodb',
-    image: mongoLogo,
-    link: 'https://www.mongodb.com'
-  }
-]
+import CreateTask from './components/CreateTask'
 
 function App() {
   const [users, setUsers] = useState([])
-  const [maxWeight, setMaxWeight] = useState(100)
-  const [tasks, setTasks] = useState([
-    { name: "Task 1", weight: 0 },
-    { name: "Task 2", weight: 0 },
-    { name: "Task 3", weight: 0 }
-  ])
 
   useEffect(() => {
     console.log(import.meta.env.VITE_API_BASE_URL)
@@ -50,58 +16,10 @@ function App() {
       .catch(console.error)
   }, [])
 
-  useEffect(() => {
-    const totalWeight = tasks.reduce((acc, task) => acc + task.weight, 0)
-    setMaxWeight(100 - totalWeight)
-  }, [tasks])
-
   return (
-    <>
-      <h1 className='italic'>
-        Hello{' '}
-        <span className='text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-          TodoDay!
-        </span>
-      </h1>
-
-      <div className='flex gap-1 justify-center p-8'>
-        {logos.map((logo, index) => {
-          return (
-            <a href={logo.link} target='_blank' rel='noreferrer' key={index}>
-              <img src={logo.image} className={`logo ${logo.name}`} alt={logo.name} />
-            </a>
-          )
-        })}
-      </div>
-
-      <div className='flex gap-2 my-10'>
-        {users.length &&
-          users.map((profile, index) => {
-            return <Profile key={index} index={index} profile={profile} />
-          })}
-      </div>
-
-      <div className='flex flex-col my-10'>
-        <p>Task weight = Importance * Expected time cost</p>
-      </div>
-
-      <div className='flex flex-col gap-2'>
-        {tasks.map((task, index) => (
-          <div key={index} className='flex items-center gap-2'>
-            <span className='w-32'>{task.name}</span>
-            <TaskWeight
-              weight={task.weight || 0}
-              setWeight={(newWeight) => {
-                const tasksCopy = [...tasks]
-                tasksCopy[index].weight = newWeight
-                setTasks(tasksCopy)
-              }}
-              maxWeight={maxWeight}
-            />
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      <CreateTask />
+    </div>
   )
 }
 
---
+++ client/src/components/CreateTask.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+function CreateTask() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const createTask = (e) => {
+    e.preventDefault();
+
+    // Do something with the task details
+    console.log('Task Name:', taskName);
+    console.log('Task Description:', taskDescription);
+
+    closeModal(); // Close the modal after creating the task
+  };
+
+  return (
+    <div className="m-8">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl"
+        onClick={openModal}
+      >
+        Create Task
+      </button>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          {/* Overlay */}
+          <div className="modal-overlay absolute inset-0 bg-black opacity-30"></div>
+
+          <div className="modal-container bg-white w-[75%] lg:w-1/3 sm:w-[80%] p-4 mx-auto rounded-2xl shadow-lg z-50 flex flex-col justify-center">
+            <div className="text-[#3c3c3c] font-bold text-xl pt-4 pb-4">Create a new Task</div>
+
+            {/* Form */}
+            <div className="modal-content pt-2 text-left px-4">
+              <form>
+                <div className="mb-2 flex items-center justify-center">
+                  <input
+                    type="text"
+                    placeholder="Task"
+                    className="appearance-none bg-white h-[3.0625rem] border-2 border-[#e5e5e5] rounded-2xl w-full py-[0.75rem] px-[1.25rem] text-slate-500 font-medium leading-tight focus:outline-none focus:shadow-outline"
+                    value={taskName}
+                    onChange={(e) => setTaskName(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <div className="mb-2">
+                  <input
+                    type="text"
+                    placeholder="Description"
+                    className="appearance-none bg-white h-[3.0625rem] border-2 border-[#e5e5e5] rounded-2xl w-full py-[0.75rem] px-[1.25rem] text-slate-500 font-medium leading-tight focus:outline-none focus:shadow-outline"
+                    value={taskDescription}
+                    onChange={(e) => setTaskDescription(e.target.value)}
+                    required
+                  />
+                </div>
+              </form>
+            </div>
+
+            {/* Buttons */}
+            <div className="flex flex-row justify-between items-center mb-4 mt-2">
+              <div className="bg-[#58cc02] border-[#58a700] hover:bg-[#79d731] hover:border-[#7abd2d] transition-colors duration-300 border-b-4 h-[2.875rem] rounded-2xl cursor-pointer z-50 px-[0.5rem] py-[0.15rem] ml-4 mr-1 flex flex-1 items-center justify-center shrink-0">
+                <button
+                  className="text-sm tracking-wider text-white font-bold uppercase w-full h-full"
+                  onClick={createTask}
+                >
+                  Create
+                </button>
+              </div>
+              <div className="bg-[#ff4b4b] border-[#f92929] hover:bg-[#ff5858] hover:border-[#ff4848] transition-colors duration-300 border-b-4 h-[2.875rem] rounded-2xl cursor-pointer z-50 px-[0.5rem] py-[0.15rem] mr-4 ml-1 flex flex-1 items-center justify-center shrink-0">
+                <button
+                  className="text-sm tracking-wider text-white font-bold uppercase w-full h-full"
+                  onClick={closeModal}
+                >
+                  Nope
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CreateTask;
