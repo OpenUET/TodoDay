6c844b56456ac3aa7df0030d7e089affce2ff38b
Trần Tuấn Anh <tuananhniichan@gmail.com>
2023-07-01 21:38:33 +0700

    Add row and column title for eisenhower matrix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,10 +21,17 @@ function App() {
     <div className="flex flex-col bg-transparent w-full h-auto">
       <div className="text-white m-8 flex items-center justify-center">Tododay</div>
       <div className="flex">
+        <div className="flex w-[30px]"></div>
+        <div className="text-white text-lg font-bold flex flex-1 items-center justify-center">Urgent</div>
+        <div className="text-white text-lg font-bold flex flex-1 items-center justify-center">Not Urgent</div>
+      </div>
+      <div className="flex">
+        <div className="w-[30px] text-white text-lg font-bold flex items-center justify-center -rotate-90">Important</div>
         <EisenBlock color={"green"} bcolor={"green"} name={"Todo now"}/>
         <EisenBlock color={"blue"} bcolor={"blue"} name={"Planning"}/>
       </div>
       <div className="flex">
+        <div className="w-[30px] whitespace-nowrap text-white text-lg font-bold flex items-center justify-center -rotate-90 width-auto">Not Important</div>
         <EisenBlock color={"red"} bcolor={"red"} name={"Someone can do it for me"}/>
         <TrashBlock/>
       </div>
--- client/src/components/TrashBlock.jsx
+++ client/src/components/TrashBlock.jsx
@@ -3,7 +3,7 @@ import { BsTrashFill } from "react-icons/bs";
 
 const TrashBlock = ({ }) => {
   return (
-    <div className="bg-[#625f5f] border-b-4 border-[#3e3d3d] p-4 m-2 rounded-2xl flex flex-1 flex-col items-center justify-start w-[512px] h-[256px]">
+    <div className="bg-[#625f5f] border-b-4 border-[#3e3d3d] p-4 m-2 rounded-2xl flex flex-1 flex-col items-center justify-start w-[512px] h-[248px]">
         <div className="text-xl font-bold uppercase mb-2">No need to do</div>
         <div className="w-full h-full bg-transparent border-[#3e3d3d] border-[3px] border-dashed rounded-2xl flex justify-center items-center">
           <BsTrashFill className="text-[#3e3d3d] text-6xl"/>
